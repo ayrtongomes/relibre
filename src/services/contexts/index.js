a2000fb6ba9969d8584101fae99d135205c0b7e3
x385282 <ayrtondcgomes@gmail.com>
2020-11-10 22:17:14 +0000

    feat:contact context

--- src/services/contexts/index.js
+++ src/services/contexts/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 // Providers
 import { AuthProvider } from '../auth';
+import { ContactProvider } from './contact';
 
-const Provider = ({ children }) => <AuthProvider>{children}</AuthProvider>;
+const Provider = ({ children }) => <AuthProvider><ContactProvider>{children}</ContactProvider></AuthProvider>;
 
 export default Provider;
